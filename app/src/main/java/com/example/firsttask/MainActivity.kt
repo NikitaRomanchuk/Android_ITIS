4d20973c9b1cc879cb8a5ad76074213ccc6fa23f
Nikita Romanchuk <ii_bk@bk.ru>
2020-09-18 21:14:15 +0300

    done second task

--- app/src/main/java/com/example/firsttask/MainActivity.kt
+++ app/src/main/java/com/example/firsttask/MainActivity.kt
@@ -2,11 +2,25 @@ package com.example.firsttask
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var nickname: EditText
+    lateinit var btnEditNick: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         print(Child().age)
+
+        nickname = findViewById(R.id.nick)
+        btnEditNick = findViewById(R.id.redprof)
+
+        btnEditNick.setOnClickListener {
+            nickname.text = nickname.text
+        }
     }
 }
+
